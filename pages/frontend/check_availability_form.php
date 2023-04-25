c7ae0cc21f0ec60eb18de65043f3de739f3c92d4
jonathannarzo <jonathan.narzo@gmail.com>
2023-04-25 11:38:40 +0800

    add hidden inputs from url parameter

--- pages/frontend/check_availability_form.php
+++ pages/frontend/check_availability_form.php
@@ -1,8 +1,15 @@
 <?php
 	$start_date = isset($_GET['start_date']) ? $_GET['start_date'] : '';
 	$end_date = isset($_GET['end_date']) ? $_GET['end_date'] : '';
+
+	$url_param_inputs = "";
+	foreach ($_GET as $key => $value)
+	{
+		$url_param_inputs .= "<input type='hidden' name='".$key."' value='". $value ."'>";
+	}
 ?>
 <form method="GET" id="room_reserve_page_form">
+	<?=$url_param_inputs?>
 	<table class="room-reservation-check-date-table">
 		<tr>
 			<td style="width:100px;">Start Date</td>
